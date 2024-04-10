2513cdb1bce0fbcf5f84eedab17811930c1eec25
KJAIMESC <104458684+KJAIMESC@users.noreply.github.com>
2024-04-10 06:00:22 -0500

    Test components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { PropiedadesComponent } from './components/propiedades/propiedades.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: 'propiedades', component: PropiedadesComponent}
+];
