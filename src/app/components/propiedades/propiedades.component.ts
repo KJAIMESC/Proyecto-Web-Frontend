4cb5877f0471fa23a63ceb0ae7f05f43798e314c
KJAIMESC <104458684+KJAIMESC@users.noreply.github.com>
2024-04-15 22:45:34 -0500

    bottones siuuu

--- src/app/components/propiedades/propiedades.component.ts
+++ src/app/components/propiedades/propiedades.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-propiedades',
@@ -9,5 +10,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './propiedades.component.css'
 })
 export class PropiedadesComponent {
-
+  constructor(private router: Router, private route: ActivatedRoute){}
+  getPropiedad() {
+    this.router.navigate(['get'], { relativeTo: this.route})
+  }
+  getAllPropiedades() {
+    this.router.navigate(['get-all'], { relativeTo: this.route})
+  }
+  createPropiedad() {
+    this.router.navigate(['create'], { relativeTo: this.route})
+  }
+  updatePropiedad() {
+    this.router.navigate(['update'], { relativeTo: this.route})
+  }
+  deletePropiedad() {
+    this.router.navigate(['delete'], { relativeTo: this.route})
+  }
 }
