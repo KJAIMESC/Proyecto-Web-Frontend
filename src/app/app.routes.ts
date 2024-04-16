82da04844819c8b169c1e4f7eb07ee5d2e28e032
KJAIMESC <104458684+KJAIMESC@users.noreply.github.com>
2024-04-15 21:26:48 -0500

    cosas funcionales :)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,28 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { PropiedadesComponent } from './components/propiedades/propiedades.component';
 import { ArrendadoresComponent } from './components/arrendadores/arrendadores.component';
-import { GetAllArrendadorComponent } from './components/get-all-arrendador/get-all-arrendador.component';
-import { SaveArrendadorComponent } from './components/save-arrendador/save-arrendador.component';
-import { UpdateArrendadorComponent } from './components/update-arrendador/update-arrendador.component';
-import { DeleteArrendadorComponent } from './components/delete-arrendador/delete-arrendador.component';
-import { GetArrendadorComponent } from './components/get-arrendador/get-arrendador.component';
+import { GetAllArrendadorComponent } from './components/arrendador-crud/get-all-arrendador/get-all-arrendador.component';
+import { SaveArrendadorComponent } from './components/arrendador-crud/save-arrendador/save-arrendador.component';
+import { UpdateArrendadorComponent } from './components/arrendador-crud/update-arrendador/update-arrendador.component';
+import { DeleteArrendadorComponent } from './components/arrendador-crud/delete-arrendador/delete-arrendador.component';
+import { GetArrendadorComponent } from './components/arrendador-crud/get-arrendador/get-arrendador.component';
+import { GetPropiedadComponent } from './components/propiedad-crud/get-propiedad/get-propiedad.component';
+import { GetAllPropiedadComponent } from './components/propiedad-crud/get-all-propiedad/get-all-propiedad.component';
+import { SavePropiedadComponent } from './components/propiedad-crud/save-propiedad/save-propiedad.component';
+import { UpdatePropiedadComponent } from './components/propiedad-crud/update-propiedad/update-propiedad.component';
+import { DeletePropiedadComponent } from './components/propiedad-crud/delete-propiedad/delete-propiedad.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
-    { path: 'propiedades', component: PropiedadesComponent },
+    { path: 'propiedades', component: PropiedadesComponent,
+        children: [
+        { path: 'get', component: GetPropiedadComponent},
+        { path: 'get-all', component: GetAllPropiedadComponent},
+        { path: 'create', component: SavePropiedadComponent},
+        { path: 'update', component: UpdatePropiedadComponent},
+        { path: 'delete', component: DeletePropiedadComponent}
+        ]
+     },
     { 
       path: 'arrendadores', 
       component: ArrendadoresComponent,
