a037af8cd1706afc245eaf339d96412745854a4d
Luisa vargas <luchisvargasgomez@gmail.com>
2024-04-16 00:45:30 -0500

    Sirve get de propiedad

--- src/app/components/propiedad-crud/get-propiedad/get-propiedad.component.ts
+++ src/app/components/propiedad-crud/get-propiedad/get-propiedad.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PropiedadService } from '../../../services/propiedad.service';
+import { Propiedad } from '../../../models/propiedad';
 
 @Component({
   selector: 'app-get-propiedad',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './get-propiedad.component.html',
-  styleUrl: './get-propiedad.component.css'
+  styleUrls: ['./get-propiedad.component.css']
 })
 export class GetPropiedadComponent {
+  propiedad: Propiedad | null = null;
+  errorMessage: string = '';
+  searchId: string = '';
 
+  constructor(private propiedadService: PropiedadService) {}
+
+  buscarPropiedad() {
+    const id = Number(this.searchId);
+    if (id) {
+      this.propiedadService.getPropiedadById(id).then((data) => {
+        this.propiedad = data;
+        this.errorMessage = '';
+      }).catch((error) => {
+        console.error(error);
+        this.propiedad = null;
+        this.errorMessage = 'Propiedad no encontrada o error en la búsqueda.';
+      });
+    } else {
+      this.errorMessage = 'Por favor ingrese un ID válido.';
+    }
+  }
+
+  onIdChange(newId: string) {
+    this.searchId = newId;
+  }
 }
--- src/app/services/propiedad.service.ts
+++ src/app/services/propiedad.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { Propiedad } from '../models/propiedad';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PropiedadService {
+  private apiUrl = 'http://localhost:8080/api/grupo1_6/proyecto1/propiedad';
 
   constructor() { }
+
+  getPropiedadById(id: number): Promise<Propiedad> {
+    return axios.get<Propiedad>(`${this.apiUrl}/${id}`).then(response => {
+      console.log("Propiedad recibida:", response.data);
+      return response.data;
+    }).catch(error => {
+      console.error("Error al obtener la propiedad", error);
+      throw error;
+    });
+  }
 }
